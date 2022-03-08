0e656e681e9d1747d8eb86f30012f4640f327889
anhtt2211 <trananh22112001@gmail.com>
2022-03-09 01:14:22 +0700

    init source

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import MainLayout from '@container/main-layout/main-layout';
 import { Provider } from 'react-redux';
 import store from '@redux/store';
+import '@styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
